31051b9debf9ef40ec62ef02374d20e08eeb9dac
Adam Gian <contact@adamgian.com>
2024-08-14 23:24:54 +1000

    feat: install docker engine

--- setup.sh
+++ setup.sh
@@ -27,6 +27,7 @@ sudo apt remove yelp
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install cmake
+sudo apt install ca-certificates
 sudo apt install curl
 sudo apt install dirmngr
 sudo apt install gawk
@@ -46,6 +47,18 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 # Dconf Editor
 sudo apt install dconf-editor
+
+# Docker Engine
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg \
+	-o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # fzf
 sudo apt install fzf
